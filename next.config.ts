d4e9ee55a1462e5533e0eee8562a347b6781980a
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2025-02-09 22:49:58 +0100

    Add nextjs example
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // The following is only configured for internal local development, you can ignore it for your apps
+  webpack: (config, { buildId, dev }) => {
+    config.resolve.symlinks = false
+    return config
+  }
 };
 
 export default nextConfig;
